e7d57df2b5a4b1bae94be01139045ae163207cde
martin122089 <martin122089@gmail.com>
2012-11-06 23:56:54 -0500

    added magnifying lens code and verified it worked

--- MagicLens/MagicLens/settings.py
+++ MagicLens/MagicLens/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'MagicLens.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'MagicLens.wsgi.application'
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = ("C:/Users/Alex/CS6456MagicLens/MagicLens/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -115,10 +115,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'techDemo',
 )
 
 # A sample logging configuration. The only tangible logging
