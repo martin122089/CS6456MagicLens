f1e5280813aa92daf5cb8926d40d109ade5162fa
martin122089 <martin122089@gmail.com>
2012-11-09 17:08:00 -0500

    changed the lens into a square to cover more area and added a smiley face to replace the image of the iphone

--- MagicLens/techDemo/urls.py
+++ MagicLens/techDemo/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('techDemo.views',
     url(r'^magnifyingGlass/$', 'magnifyingGlass'),
+    url(r'^screenDarkening/$', 'screenDarkening'),
 )
