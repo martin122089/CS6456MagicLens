75cb7682819d63f33fd9ca83da13181c853fe4f2
martin122089 <martin122089@gmail.com>
2012-11-13 22:28:45 -0500

    can now move lens around and resize it

--- MagicLens/techDemo/urls.py
+++ MagicLens/techDemo/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('techDemo.views',
     url(r'^magnifyingGlass/$', 'magnifyingGlass'),
-    url(r'^screenDarkening/$', 'screenDarkening'),
     url(r'^screenDimExample/$', 'screenDimExample'),
 )
--- MagicLens/techDemo/views.py
+++ MagicLens/techDemo/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render_to_response
 def magnifyingGlass(request):
     return render_to_response('magnifyingGlass.html', {})
 
-def screenDarkening(request):
-    return render_to_response('screenDarkening.html', {})
-
 def screenDimExample(request):
     return render_to_response('screenDimExample.html', {})
